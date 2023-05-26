0cde59292a4c699e01ee0902439354ccfe3c1315
Philtell <2267507789@qq.com>
2023-05-26 10:17:47 +0800

    Delete src/cmake-build-debug directory

--- src/cmake-build-debug/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/philtell/code/gaode_ws/src
-# Build directory: /home/philtell/code/gaode_ws/src/cmake-build-debug
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("gtest")
-subdirs("GDCamera")
--- src/cmake-build-debug/GDCamera/CMakeFiles/GDCamera_node.dir/compiler_depend.make
+++
@@ -1,2 +0,0 @@
-# Empty compiler generated dependencies file for GDCamera_node.
-# This may be replaced when dependencies are built.
